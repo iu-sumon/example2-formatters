7d2dcbd5a07a8e4505499a33b9ae23e5f693cc4a
Ashna <joyotrii@yahoo.com>
2023-02-05 10:42:57 +0600

    Slickgrid Examples Test

--- js/slick.formatters.js
+++ js/slick.formatters.js
@@ -46,7 +46,7 @@
   }
 
   function CheckmarkFormatter(row, cell, value, columnDef, dataContext) {
-    return value ? "<img src='../images/tick.png'>" : "";
+    return value ? "<img src='./images/tick.png'>" : "";
   }
 
   // exports
